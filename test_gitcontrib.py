703b8a467c9aa867b08ce72c9be9d09e60b40740
Nick Frost <nickfrostatx@gmail.com>
2015-12-07 18:31:51 -0800

    Use actual namespacing

--- test_gitcontrib.py
+++ test_gitcontrib.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 """Test them contribs."""
 
-from gitcontrib import usage, git
-from subprocess import check_call
+import gitcontrib
 import pytest
+import subprocess
 
 
 @pytest.fixture
 def git_repo(tmpdir):
-    check_call(['git', 'init', str(tmpdir)])
+    subprocess.check_call(['git', 'init', str(tmpdir)])
     return tmpdir
 
 
 def test_usage(capsys):
-    usage()
+    gitcontrib.usage()
     out, err = capsys.readouterr()
     assert err == 'Usage:\ngitcontrib [path] [extension ...]\n'
 
 
 def test_git(git_repo):
-    assert 'nothing to commit' in git(str(git_repo), 'status')
+    assert 'nothing to commit' in gitcontrib.git(str(git_repo), 'status')
