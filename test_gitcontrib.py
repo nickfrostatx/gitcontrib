59d1d9cf834ee8b0b41398d03381cd33562d7574
Max Zinkus <mzinkus@calpoly.edu>
2016-04-25 16:27:40 -0700

    Fix json_print => jsonify in tests

--- test_gitcontrib.py
+++ test_gitcontrib.py
@@ -38,8 +38,6 @@ def test_json(capsys):
     total = 20
     auth = {'a': 12, 'b': 2, 'c': 1, 'd': 5}
     expect = 0.25
-    gitcontrib.json_print(total, auth, expect)
-    out, err = capsys.readouterr()
-    j_data = json.loads(out)
+    j_data = gitcontrib.jsonify((total, auth, expect)
     assert j_data['a']['met_expected']
     assert j_data['b']['lines'] == 2
